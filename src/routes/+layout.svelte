88ea8c11fb5009db8c748fa58154024ff793a1ac
Andres Oliveira <andresoliveira@protonmail.com>
2024-05-09 12:12:18 -0300

    Layout scrool to top button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,28 @@
 <script>
   import "../app.css";
+	import Footer from "../components/Footer.svelte";
+	import Header from "../components/Header.svelte";
+
+  let y = 0
+  let innerHeight = 0
+  let innerWidth = 0
+
+  function goTop() {
+    document.body.scrollIntoView()
+  }
 </script>
 
-<slot />
+<div class="relative flex flex-col max-w=[1400] mx-auto w-full text-sm s:text-base min-h-screen">
+    <div class={"fixed bottom-0 w-full duration-200 flex p-10 z=[10]" + (
+      y > 0 ? 'opacity-full pointer-events-auto' : 'pointer-events-none opacity-0'
+    )}>
+      <button>
+        <i class="fa-solid fa-arrow-up" />
+      </button>
+    </div>
+    <Header />
+    <slot />
+    <Footer />
+</div>
+
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth/>
