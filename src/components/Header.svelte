85b82751a6c43a209237524127b3494cdf7dd795
Andres Oliveira <andresoliveira@protonmail.com>
2024-05-10 10:29:54 -0300

    Header Hero and image

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -7,7 +7,7 @@
   ]
 
 </script>
-<header class={"sticky z=[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid" + 
+<header class={"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid" + 
 (
   y > 0 ?
     " py-4 bg-slate-950 border-violet-950"
@@ -25,8 +25,9 @@
   </div>
   <a 
     href="#"
-    class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
-    <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover: translate-x-full z-0 duration-200"/>
+    target="_blank"
+    class="hover:bg-violet-400 hover:opacity-100 blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
+    <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 gro translate-x-full z-0 duration-200"/>
       <h4 class="relative z-9">Get in touch</h4>
   </a>
 </header>
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,1 +1,30 @@
-<h1>Main</h1>
+<script>
+
+</script>
+<main class="flex flex-col flex-1 p-4">
+  <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+    <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+      <h2 class="font-semibold text-4xl smm:text-5xl md:text-6xl">
+        Olá! Sou <span class="text-violet-400">Andres</span> Oliveira <br/>
+        Dev <span class="text-violet-400">Front End</span>
+      </h2>
+      <p class="text-base sm:text-lg md:text-xl">
+        Minha <span class="text-violet-400">linguagem principal é</span> Javascript <br/>
+        Frameworks e Libraries: <span class="text-violet-400">ReactJS, VueJS, NextJS e Svelte.</span>
+      </p>
+      <a
+        href="#"
+        target="_blank"
+        class="hover:bg-violet-400 hover:opacity-100 blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl
+        relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950
+        cursor-pointer"
+      >
+        <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover: translate-x-full z-0 duration-200"/>
+        <h4 class="relative z-9">Get in touch &rarr;</h4>
+      </a>
+    </div>
+    <div class="relative shadow-2x1 grid place-items-center">
+      <img src={"images/profile.jpg"} alt="Profile pic" class="object-cover z=[2] max-h-[70vh]" />
+    </div>
+  </section>
+</main>
