8294a4cf4d85acf96dde78453dadd9bd8b3e1bcd
Andres Oliveira <andresoliveira@protonmail.com>
2024-05-17 10:14:29 -0300

    Layout fixed (particles), new footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,19 +1,22 @@
-<footer
-  class="py-20 sm:py-32 bg-black border-top border-solid border-violet-950
-  flex flex-col gap-4 sm:gap-8 justify-center items-center"
->
-  <p class="text-slate-50 text-2xl">
-    Entre em <span class="text-primary">contato</span>.
-  </p>
-  <div class="flex flex-col gap-4 items-center justify-center">
-    <p><b class="pr-2">Email</b> andresoliveira@protonmail.com</p>
-    <p><b class="pr-2">Linkedin</b> https://www.linkedin.com/in/dezoliveira</p>
-    <p><b class="pr-2">Github</b>
-      <a href="https://www.github.com/dezoliveira" class="text-primary" target="_blank">
-        @dezoliveira<sup><span class="text-xs scale-75 pl-0.5">
-          <i class="fa-solid fa-arrow-up-right-from-square text-xs scale=[75%]"/>
-        </span></sup>
-      </a>
-    </p>
+<footer class="footer footer-center p-10 bg-base-200 text-base-content rounded">
+  <div>
+    <img alt="svelte-icon" src="favicon.png" class="w-[48px] text-purple-400"/>
+    <h1 class="text-2xl">Feito com <span class="text-primary">Svelte</span></h1>
   </div>
+  <nav>
+    <div class="grid grid-flow-col gap-4">
+      <a href="https://github.com/dezoliveira" target="_blank">
+        <i class="fa-brands fa-github  text-2xl hover:text-secondary"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/dezoliveira" target="_blank">
+        <i class="fa-brands fa-linkedin  text-2xl hover:text-secondary"></i>
+      </a>
+      <a href="mailto: andresoliveira@protonmail.com" target="_blank">
+        <i class="fa-solid fa-paper-plane text-2xl hover:text-secondary"></i>
+      </a>
+    </div>
+  </nav> 
+  <aside>
+    <p>Copyright © 2024 - Todos os direitos reservados by <span class="text-primary">Andres Oliveira</span></p>
+  </aside>
 </footer>
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -81,6 +81,7 @@
     </div>
   </section>
 
+
   <section id="projects" class="py-20 lg:py-32 flex flex-col gap-24 p-4">
     <div class="flex flex-col gap-2 text-center">
       <h6 class="text-lg sm:text-xl md:text-2xl">
