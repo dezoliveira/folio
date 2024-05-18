b8e110880d11d66f11e6227e35a7e19f82b7b00e
Andres Oliveira <andresoliveira@protonmail.com>
2024-05-18 09:55:20 -0300

    github link fixed and stats bar

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -92,8 +92,9 @@
         trabalho ?
       </h3>
     </div>
+    
     <a
-      href="https://www.github.com/dezoilveira"
+      href="https://www.github.com/dezoliveira"
       target="_blank"
       class="mx-auto px-4 py-2 flex items-center gap-2 -mb-4 sm:-mb-0
       -mt-10 ">
@@ -114,7 +115,40 @@
         </Step>
       {/each}
     </div>
+    <div class="stats shadow">
+      <div class="stat">
+        <div class="stat-figure text-primary">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+        </div>
+        <div class="stat-title">Total de Projetos</div>
+        <div class="stat-value text-primary">+78</div>
+        <div class="stat-desc">5 em andamento</div>
+      </div>
+      
+      <div class="stat">
+        <div class="stat-figure text-secondary">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+        </div>
+        <div class="stat-title">Total de commits</div>
+        <div class="stat-value text-secondary">+722</div>
+        <div class="stat-desc">+250 contribuiçoes em 2024</div>
+      </div>
+      
+      <div class="stat">
+        <div class="stat-figure text-secondary">
+          <div class="avatar online">
+            <div class="w-16 rounded-full">
+              <img src="images/profile.png" />
+            </div>
+          </div>
+        </div>
+        <div class="stat-value">70%</div>
+        <div class="stat-title">uso de Frameworks</div>
+        <div class="stat-desc text-secondary">30% de Javascript</div>
+      </div>
+    </div>
   </section>
+
   <section
     id="about"
     class="py-20 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative">
